33741b12b7b5189c26670cb43b59bf25c3917018
Jin Cai <Gene@MP09.local>
2020-05-30 09:45:12 -0700

    read_csv

--- Data/DataProvider.py
+++ Data/DataProvider.py
@@ -1,12 +1,19 @@
 
+import pandas as pd
 
 class DataProvider():
 
-    def holidays():
+    def holidays(self):
+        return pd.read_csv('Data/Holidays.csv')
+
+    def vacations(self):
+        return pd.read_csv('Data/Vacations.csv')
+
+    def work_from_home(self):        
         pass
 
-    def vacations():
-        pass
+if __name__ == '__main__':
 
-    def work_from_home():        
-        pass
+    dp = DataProvider()
+    print(dp.holidays()) 
+    print(dp.vacations()) 
