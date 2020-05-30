9632196f59df757217175fd918362eca7129f104
Jin Cai <Gene@MP09.local>
2020-05-30 11:00:59 -0700

    populate dataframe

--- Data/DataProvider.py
+++ Data/DataProvider.py
@@ -4,16 +4,39 @@ import pandas as pd
 class DataProvider():
 
     def holidays(self):
-        return pd.read_csv('Data/Holidays.csv')
+        h = pd.read_csv('Data/Holidays.csv')
+        return pd.to_datetime(h.Holiday)
+
 
     def vacations(self):
-        return pd.read_csv('Data/Vacations.csv')
+        v = pd.read_csv('Data/Vacations.csv')
+        v.Vacation = pd.to_datetime(v.Vacation)
+        return v
 
     def work_from_home(self):        
-        pass
+        w = pd.read_csv('Data/WFH.csv')
+
+        tempSeries = pd.Series()
+
+        for row in w.iterrows():
+            if '~' in row[1].WFH:
+                dateRange = row[1].WFH.split('~')
+                s = pd.date_range(start = dateRange[0], end=dateRange[1]).to_series()                
+                
+                w = w.drop(index = row[0])
+                tempSeries = tempSeries.append(s)
+
+        tempSeries = w.WFH.append(tempSeries)
+
+        return pd.to_datetime(tempSeries)
+
+
 
 if __name__ == '__main__':
 
     dp = DataProvider()
-    print(dp.holidays()) 
-    print(dp.vacations()) 
+
+    # print(dp.holidays()) 
+    # print(dp.vacations()) 
+    
+    print(dp.work_from_home().head(10))
--- Main.py
+++ Main.py
@@ -1,2 +1,21 @@
-import datetime
+import pandas as pd
+
+from Data.DataProvider import DataProvider
+dp = DataProvider()
+
+s = pd.date_range(start='2020-1-1', end='2020-12-31').to_series()
+df = pd.DataFrame(s , columns=['Date'] )
+df['DayOfWeek'] = df['Date'].dt.weekday
+
+df['IsHoliday'] = df.Date.isin(dp.holidays())
+
+df['OnVacation'] = df.Date.isin(dp.vacations().Vacation)
+
+df['WorkingFromWA'] = df.Date.isin(dp.work_from_home())
+
+print( df.iloc[90:150])
+
+
+
+
 
