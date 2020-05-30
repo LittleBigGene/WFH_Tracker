7d10b0c882b6f247bf1b0c7c8bc436f24d4e655a
Jin Cai <Gene@MP09.local>
2020-05-30 11:15:51 -0700

    calculate wfh days

--- Main.py
+++ Main.py
@@ -3,19 +3,22 @@ import pandas as pd
 from Data.DataProvider import DataProvider
 dp = DataProvider()
 
-s = pd.date_range(start='2020-1-1', end='2020-12-31').to_series()
-df = pd.DataFrame(s , columns=['Date'] )
-df['DayOfWeek'] = df['Date'].dt.weekday
+full_year = pd.DataFrame(pd.date_range(start='2020-1-1', end='2020-12-31').to_series(), columns=['Date'] )
 
-df['IsHoliday'] = df.Date.isin(dp.holidays())
+full_year['DayOfWeek'] = full_year['Date'].dt.weekday
 
-df['OnVacation'] = df.Date.isin(dp.vacations().Vacation)
+full_year['IsHoliday'] = full_year.Date.isin(dp.holidays())
 
-df['WorkingFromWA'] = df.Date.isin(dp.work_from_home())
+full_year['OnVacation'] = full_year.Date.isin(dp.vacations().Vacation)
 
-print( df.iloc[90:150])
+full_year['WorkingFromWA'] = full_year.Date.isin(dp.work_from_home())
 
 
+working_calendar = full_year.query('DayOfWeek < 5 & not OnVacation & not IsHoliday') 
+
+wfhDays = working_calendar.query('WorkingFromWA')
+
+print(working_calendar.count())
 
 
 
