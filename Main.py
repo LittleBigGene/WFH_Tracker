cda4e08570ed8318a4911a7745dcdfa59e8e6bef
jin cai <genecai@gmail.com>
2020-05-30 19:05:20 -0700

    touch-up

--- Main.py
+++ Main.py
@@ -7,18 +7,18 @@ full_year = pd.DataFrame(pd.date_range(start='2020-1-1', end='2020-12-31').to_se
 
 full_year['DayOfWeek'] = full_year['Date'].dt.weekday
 
-full_year['IsHoliday'] = full_year.Date.isin(dp.holidays())
+full_year['Holiday'] = full_year.Date.isin(dp.holidays())
 
 full_year['OnVacation'] = full_year.Date.isin(dp.vacations().Vacation)
 
 full_year['WorkingFromWA'] = full_year.Date.isin(dp.work_from_home())
 
 
-working_calendar = full_year.query('DayOfWeek < 5 & not OnVacation & not IsHoliday') 
+working_calendar = full_year.query('DayOfWeek < 5 & not OnVacation & not Holiday') 
 
-wfhDays = working_calendar.query('WorkingFromWA')
+wfhDays = working_calendar[working_calendar.WorkingFromWA].Date.count()
 
-print(working_calendar.count())
+print(f'There are total {working_calendar.Date.count()} working days in 2020, of which {wfhDays} days are WFH, {round(wfhDays/working_calendar.Date.count()*100,2)}%')
 
 
 
